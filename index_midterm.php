fcfea104efe6b85b87cc2074d1f5c5ef5495e338
Yuqi Zhang <zyq645@gmail.com>
2015-10-22 15:34:26 -0500

    Final

--- index_midterm.php
+++ index_midterm.php
@@ -9,8 +9,14 @@ if (mysqli_connect_errno()) {
  }
  
 	if(isset($_POST['name'])) {
+
+
+
+
 	
 //MIDTERM ADDITIONS - EXPERT TIP - AVOID POSTING LOOP
+//Student Comment: Initial variable UNAME, GREETING and CARRCOUNT for furture reference first
+
 	 if(isset($_POST['cookie'])) {
 	  $COOKIELOAD=1; }
 	  
@@ -18,7 +24,11 @@ if (mysqli_connect_errno()) {
 	     $UNAME = ($_POST['name']);
 		 $GREETING = 'Welcome back '. $UNAME.'.';
 		 
-	//MIDTERM ADDITIONS - SQL SELECT TO GET USER DETAILS	
+//MIDTERM ADDITIONS - SQL SELECT TO GET USER DETAILS	
+// Student Comment: Using SQL to fetch back-end consumer data. It first establish a mysql query that extract
+// all the data related to the user who logs on. And assign the exact value of CARTCOUNT, PREF and
+// LATEST to replace the initial value
+
 		 $search1 = mysqli_query($con,"SELECT * FROM `Customer` WHERE UID = '". $UNAME ."'");
 		 if(mysqli_num_rows($search1) > 0){
 		 while($row = mysqli_fetch_array($search1)) {
@@ -26,7 +36,19 @@ if (mysqli_connect_errno()) {
 		  $PREF = $row[Pref];
 		  $LATEST = $row[LastCart];
 		  }
-	//MIDTERM ADDITIONS - LOGIC TO SET BOOKS
+		  
+		  
+//MIDTERM ADDITIONS - LOGIC TO SET BOOKS
+//Student Comment: Using SQL to fetch beack-end book info and use if loop to match book infor. It begins with creating 
+//a query that fetch all the books info in the selected category. The next step is examine the status of the logged-in 
+//consumer. If he/she has recently place items in the cart, it will assign n equals to 2 and build a new query to take 
+//and display the info of the first book from query3. (Query 3 take all books that are recently placed) However, if 
+//he/she doesn't place anything, it will have 2 situations and n will be assigned as 1. If the book is not lastest placed, customers will see the 
+//info this book in the category. But if this book is recently placed, the system will show the book with id of 100,
+//200, 300 and 400
+
+
+
 	      $search2 = mysqli_query($con,"SELECT * FROM `Bookdetails` WHERE CatID = '". $PREF ."'");
 		  if($LATEST != 0) {
 		   $n=2;
@@ -68,7 +90,10 @@ if (mysqli_connect_errno()) {
       }
      }	  else { 
 		 $GREETING = 'Welcome Guest. <a href="#" class="my_popup_open">Log on</a> for recommendations.';
-	//MIDTERM ADDITIONS - SET BOOKS FOR LOGGED OUT VISITORS
+		 
+//MIDTERM ADDITIONS - SET BOOKS FOR LOGGED OUT VISITORS
+//Student Comment: Show the info of the default books with id 100,200,300,400 to logged out customers
+	
 		 $n=1;
 		 $search4 = mysqli_query($con,"SELECT * FROM `Bookdetails` WHERE bid in (100,200,300,400)");
            while($row = mysqli_fetch_array($search4)) {
@@ -188,7 +213,17 @@ if (mysqli_connect_errno()) {
 
    $.fn.DeetsBox = function(bid) {
         if(bid == '1'){	
-	//MIDTERM ADDITIONS - NEW VARIABLES AND CONDITIONS
+
+//MIDTERM ADDITIONS - NEW VARIABLES AND CONDITIONS
+//Student Comment: This is a Java code. It first creates 2 new variable: book name and book price
+//If the book id is 1, it will returns the name and price of book1.
+//And it will put name and price in pop-up window and bookshelf.
+//If the book is already in the cart, the reminder will be purchase; otherwise it will be learn more.
+//The same logic applies to book2-4.
+
+
+
+
 		var bookname = $( "#book1" ).val();
 		var bookprice = $( "#book1price" ).val();
 		$("#showbookdeets").html(bookname + "<p>" + bookprice); 
@@ -243,6 +278,7 @@ if (mysqli_connect_errno()) {
 <script language="JavaScript">
 
 //TWO FUNCTIONS TO SET THE COOKIE
+//Student Comment: This is Java Script. Define mixCookie function by setting the name to cookie
 
 function mixCookie() {
 
@@ -252,6 +288,8 @@ function mixCookie() {
 			
    }
    
+//Student Comment:This is Java Script. Define bakeCookie function by setting the created time, expired time and documented format to cookie  
+
 function bakeCookie(cname, cvalue1, cvalue2, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -260,17 +298,31 @@ function bakeCookie(cname, cvalue1, cvalue2, exdays) {
 }
 
 //TWO FUNCTIONS TO GET THE COOKIE
+//Student Comment: This is Java Script. 
 
 function checkCookie() {
     var userdeets = getCookie("readuser");
+    
 //MIDTERM ADDITIONS - 'CHECKFIRST' VARIABLE - FOR 'IF' BELOW
-	var checkfirst = document.getElementById('firstload').value;
+//Student Comment: The function fo "checkfirst" is to see whether we have cookie and info about 
+//the logged customer. If it is not the first time the customer visit and log (we examine this through if statement), 
+//it will read the stored cookie and make personalized change for the customer.
 
+
+	var checkfirst = document.getElementById('firstload').value;
+	
+	
+	
+//Student Comment: if the entered name is no missing, the system will return the reformatted, splited
+//name and personalized greeting message. If it's missing, it will return blank.
     if (userdeets != "") {
 	    var deets = userdeets.split("%-");
 		var user = deets[0];
 		
-//MIDTERM ADDITIONS - NEW NESTED 'IF' LOGIC TO POST USERNAME TO PHP TO CHECK FOR DETAILS THROUGH SQL		
+//MIDTERM ADDITIONS - NEW NESTED 'IF' LOGIC TO POST USERNAME TO PHP TO CHECK FOR DETAILS THROUGH SQL	
+//Student Comment: The function of this if statement has been explained in ROW 307-309. If checkfirst is 
+//not equal to 1, it means it is not the first the customer log and we have info about this person and 
+//we can make personalized changes.	
 		 if(checkfirst != 1){
 		  
 		  post('index.php',{name:user,cookie:yes});
@@ -279,6 +331,8 @@ function checkCookie() {
 	     
   } else { return "";}
 }
+
+//Student Comment: This is a JAVA Script. It helps format user name.
 
 function getCookie(cname) {
 
@@ -291,7 +345,8 @@ function getCookie(cname) {
     return "";
 }
 
-<!--MIDTERM ADDITIONS - FUNCTION TO DELETE COOKIE -->
+<!--MIDTERM ADDITIONS - FUNCTION TO DELETE COOKIE-->
+
 
 function drop_cookie(name) {
   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
@@ -299,6 +354,7 @@ function drop_cookie(name) {
 }
 
 <!--MIDTERM ADDITIONS - FUNCTION TO POST FROM JS -->
+
 function post(path, params, method) {
     method = method || "post"; // Set method to post by default if not specified.
 
@@ -349,11 +405,19 @@ function post(path, params, method) {
  <img src="img/ic1.jpg" style="max-height: 100%;">
  
 <!--MIDTERM ADDITIONS - LOG-OUT LINK & LOGIC FOR VISITOR LOGGED STATE. CART NOW A LINK.--> 
+
 <?php if(isset($_POST['name'])) { ?>
+<!-- Student Comment: It is not meaningful to track any "LOG OUT" event, so I didn't add any tracking to this event -->	
+
     <div style="float:right; margin-right:50px;margin-top:10px; color:white;"> <a href="#" style="color:white;" onclick="drop_cookie('readuser');">Log Out</a> </div>
 	
 	<div style="float:right; margin-right:75px;margin-top:10px; color:white;"> 
-	 <a href="#" style="color:white;" >Cart: <?php echo $CARTCOUNT ?></a>
+	
+<!-- Student Comment: Add tracking code to Cart, tracking everytime the cart is activated by customer. The category
+	 of this action is "convert" and is called "cart_click". I embed a php code inside of the ga statement. So it 
+	 will return the user name of people who log on -->	
+	 
+	 <a href="#" style="color:white;" onClick="ga('send', 'event', 'convert', 'cart_click', '<?php echo $UNAME ?>');">Cart: <?php echo $CARTCOUNT ?></a>
 	 </div>
  <?php } ?>
  </div>
@@ -365,6 +429,9 @@ function post(path, params, method) {
  <div id="greeting"> <?php echo $GREETING ?> </div>
  
  <!--MIDTERM ADDITIONS - NEW HIDDEN FIELD - USED IN NEW CHECKCOOKIE LOGIC -->
+ <!--Student Comment: The following two rows tell us whether this cookie about this person
+ and whether the book is carted-->
+ 
  <input type="hidden" id="firstload" value="<?php echo $COOKIELOAD ?>">
  
   <!--MIDTERM ADDITIONS - NEW HIDDEN FIELD - USED FOR BOOK1 CTA -->
@@ -375,22 +442,30 @@ function post(path, params, method) {
  
  <div id="cta1"> Please browse our options:</div>
  <section>
- <div id="one" style="padding:10px;">
+ <!--Student Comment: Slot1-->
+ <!--Student Comment: Set propertities of the section-->
+ <div id="one" style="padding:10px;"> 
+ <!--Student Comment: Use PHP to get book1 info-->
 	<?php echo $BOOK1; ?>
+ <!--Student Comment: Use PHP to get book1 pic. Change PHP VARIABLE TO BOOKPIC1 TO SPECIFY. The same logics applies to other slot-->
 	<img src="img/<?php echo $BOOKPIC1 ?>" style="float:left; margin-right:6px; height: 100px;">
 	
-<!-- MIDTERM ADDITIONS - ADDED BOOKPRICE. MADE BOOK DYNAMIC -->
+<!-- MIDTERM ADDITIONS - ADDED BOOKPRICE. MADE BOOK DYNAMIC of Book1 in the pop up window via embedding php language in html. 
+Specify id and value to book1 and book1price. The same logics applies to other books-->
     <input type="hidden" id="book1" value="<?php echo $BOOKTITLE1 ?>">
 	<input type="hidden" id="book1price" value="<?php echo $BOOKPRICE1 ?>">
-	
+
+<!-- Student Comment: ADDED booki title, author, description via embedding php language and specifying to book1. The same logics applies to other books-->	
 	<strong><?php echo $BOOKTITLE1 ?></strong><p>
 	by <?php echo $BOOKAUTH1 ?> <p>
 	<?php echo $BOOKDESC1 ?>
 	<p>
 	<?php if($LATEST != 0){ ?>
-<!-- Student Comment: Add tracking code to the purchase bottom and also
-	activate the deetsbox function by adding ";" -->
-	<input type="button" value="Purchase" id="book1button" onClick="ga('send', 'event', 'browse', 'purchase', document.getElementById('book1').value); $(this).DeetsBox(1);">
+	
+	
+<!-- Student Comment: Add tracking code to the purchase botton and also
+	activate the deetsbox function by adding ";". Label Book name to G.A-->
+	<input type="button" value="Purchase" id="book1button" onClick="ga('send', 'event', 'convert', 'purchase', document.getElementById('book1').value); $(this).DeetsBox(1);">
 	<?php } else { ?>
 	<input type="button" value="Learn More" id="book1button" onClick="ga('send', 'event', 'browse', 'learn_more_home', document.getElementById('book1').value); $(this).DeetsBox(1);">
 	<?php } ?>
@@ -398,10 +473,11 @@ function post(path, params, method) {
 	
 <!-- Student Comment: Modify Slot2 -->
  <div id="two" style="padding:10px;">
+<!--Student Comment: Use PHP to get book2 info-->
 	<?php echo $BOOK2; ?>
 <!-- Student Comment: Use PHP language to extract and display pic related to Book2 from backstage dataset-->	
 	<img src="img/<?php echo $BOOKPIC2 ?>" style="float:left; margin-right:6px; height: 100px;">
-<!-- Student Comment - Use PHP language to added bookprice. made book dynamic for slot2 from backstage dataset -->
+<!-- Student Comment - Use PHP language to added bookprice. made book dynamic for slot2  in pop up window from backstage dataset -->
     <input type="hidden" id="book2" value="<?php echo $BOOKTITLE2 ?>">
     <input type="hidden" id="book2price" value="<?php echo $BOOKPRICE2 ?>">
     
@@ -412,16 +488,19 @@ function post(path, params, method) {
 <!-- Student Comment - Use PHP to added Slot2 Description from backstage dataset-->
 	<?php echo $BOOKDESC2 ?>
 	<p>
-	<input type="button" value="Learn More" id="book2button" onClick="$(this).DeetsBox(2)";>
+<!-- Student Comment: Add tracking code to the learn more botton Label Book name to G.A -->
+	<input type="button" value="Learn More" id="book2button" onClick="ga('send', 'event', 'browse', 'learn_more_home', document.getElementById('book2').value); $(this).DeetsBox(2);">
 	</div>
 	
 	
 <!-- Student Comment: Modify Slot3 -->	
  <div id="three" style="padding:10px;">
+<!--Student Comment: Use PHP to get book3 info-->
+	<?php echo $BOOK2; ?>
 	<?php echo $BOOK3; ?>
 <!-- Student Comment: Use PHP language to extract and display pic related to Book3 -->	
 	<img src="img/<?php echo $BOOKPIC3 ?>" style="float:left; margin-right:6px; height: 100px;">
-<!-- Student Comment - Added bookprice. made book dynamic for slot3 -->
+<!-- Student Comment - Added bookprice. made book dynamic for slot3  in the pop up window-->
     <input type="hidden" id="book3" value="<?php echo $BOOKTITLE3 ?>">
     <input type="hidden" id="book3price" value="<?php echo $BOOKPRICE3 ?>">
     
@@ -432,7 +511,8 @@ function post(path, params, method) {
 <!-- Student Comment - Added Slot3 Description -->
 	<?php echo $BOOKDESC3 ?>
 	<p>
-	<input type="button" value="Learn More" id="book3button" onClick="$(this).DeetsBox(3)";>
+<!-- Student Comment: Add tracking code to the learn more botton Label Book name to G.A-->
+	<input type="button" value="Learn More" id="book3button" onClick="ga('send', 'event', 'browse', 'learn_more_home', document.getElementById('book3').value); $(this).DeetsBox(3);">
 	</div>
     
     
@@ -442,6 +522,7 @@ function post(path, params, method) {
 <?php 
 if($n > 4){ ?>
  <div id="four" style="padding:10px;">
+ <!--Student Comment: Use PHP to get book4 info-->
 	<?php echo $BOOK4; ?>
 <!-- Student Comment: Use PHP language to extract and display pic related to Book4 -->	
 	<img src="img/<?php echo $BOOKPIC4 ?>" style="float:left; margin-right:6px; height: 100px;">
@@ -456,7 +537,8 @@ if($n > 4){ ?>
 <!-- Student Comment - Added Slot4 Description -->
 	<?php echo $BOOKDESC4 ?>
 	<p>
-	<input type="button" value="Learn More" id="book4button" onClick="$(this).DeetsBox(4)";>
+<!-- Student Comment: Add tracking code to the learn more botton Label Book name to G.A-->
+	<input type="button" value="Learn More" id="book4button" onClick="ga('send', 'event', 'browse', 'learn_more_home', document.getElementById('book4').value); $(this).DeetsBox(4);">
 	</div>
 	<?php } else { ?>
 	<div id="four" style="padding:10px;"></div>
@@ -479,6 +561,7 @@ if($n > 4){ ?>
     <input type="hidden" id="bookshelf"  value="0">
 	
 <!--MIDTERM ADDITIONS - CHANGED TO BUTTON TO CLOSE-->
+<!-- Student Comment: Add tracking code for every time customer add an item to the cart. Label # of item to G.A-->
 
 	<button id="deetcta" class="bookdeets_close"  onClick="ga('send', 'event', 'convert', 'cart_add', document.getElementById('bookshelf').value)";/>Add to Cart</button> <p>
 	</div>
